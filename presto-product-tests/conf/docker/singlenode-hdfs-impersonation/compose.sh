81a453b28a0da3e9ab63b98ef15ef80d62509a4c
Grzegorz Kokosiński <Grzegorz.Kokosinski@Teradata.com>
2016-10-26 09:02:44 +0200

    Split standard compose file into: base, hive and presto

--- presto-product-tests/conf/docker/singlenode-hdfs-impersonation/compose.sh
+++ presto-product-tests/conf/docker/singlenode-hdfs-impersonation/compose.sh
@@ -6,7 +6,9 @@ TEMPTO_CONFIG_YAML_DEFAULT="${PRODUCT_TESTS_ROOT}/conf/tempto/tempto-configurati
 export TEMPTO_CONFIG_YAML=$(canonical_path ${TEMPTO_CONFIG_YAML:-${TEMPTO_CONFIG_YAML_DEFAULT}})
 
 docker-compose \
--f ${BASH_SOURCE%/*}/../common/standard.yml \
+-f ${BASH_SOURCE%/*}/../common/base.yml \
+-f ${BASH_SOURCE%/*}/../common/hive.yml \
+-f ${BASH_SOURCE%/*}/../common/presto.yml \
 -f ${BASH_SOURCE%/*}/../common/jdbc_db.yml \
 -f ${BASH_SOURCE%/*}/docker-compose.yml \
 "$@"
